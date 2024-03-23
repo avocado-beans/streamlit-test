88b70da43eee7777844c07b0d4d07fe52f504a1d
estifanos <orthodoxrabbibillclinton42@gmail.com>
2024-03-23 17:52:02 +0300

    folium streamlit test

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import streamlit as st
+import folium
+from streamlit_folium import st_folium
 from modules import *
-from streamlit_bcp import back_camera_input
+from ai import askAI21
+from streamlit_back_camera_input import back_camera_input
 
 image = back_camera_input()
 if image:
@@ -9,4 +12,8 @@ if image:
           file.write(image.getbuffer())
     plantName = getPlant('img.jpg')
     st.header(plantName)
+    response = askAI21("Q: Tell me about the plant species known as "+plantName+". \nA:")
+    st.subheader(response)
+    
+    
 

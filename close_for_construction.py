54c9a832a7e285317b2b060e92f98d311ca3e741
stevie <122219518+avocado-beans@users.noreply.github.com>
2024-03-27 18:40:58 +0300

    Update close_for_construction.py

--- close_for_construction.py
+++ close_for_construction.py
@@ -4,7 +4,7 @@ st.subheader("I'm modifying it until the day of the contest so it'll be down for
 st.write("Thanks for visiting the site, though! Trust me, this project will not disappoint :)")
 st.write("I'm @tae2big on instagram if you want to contact me until then.")
 st.subheader("Q: What is PLANTR?")
-st.write("Agriculture is a very dominant part of Ethiopia's economy, just like how it is with most other developing countries. We have recently started to use a more modern and technology-based approach when it comes to farming; and this project aims to play a part in that.  \n  \nPlantr is a project that combines a chatGPT-like LLM (AI21), an image recognition API (Pl@ntNet) and geo-spatial technology to first recognize and identify plants from pictures and then give the user information about how the plant can be taken care of, how it could be used, etc. And then it gives analysis on how the addition of that species to a novel ecosystem could affect ecosystem.")
+st.write("Agriculture is a very dominant part of Ethiopia's economy, just like how it is with most other developing countries. We have recently started to use a more modern and technology-based approach when it comes to farming; and this project aims to play a part in that.  \n  \nPlantr is a project that combines a chatGPT-like LLM (AI21), an image recognition API (Pl@ntNet) and geo-spatial technology to first recognize and identify plants from pictures and then give the user information about how the plant can be taken care of, how it could be used, etc. And then it gives analysis on how the addition of that species to a novel ecosystem could affect said ecosystem.")
 st.divider()
 left, right = st.columns(2)
 with right:

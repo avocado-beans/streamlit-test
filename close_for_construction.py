ed7b168e45ba02a3b7347b28bfcce90eba306593
stevie <122219518+avocado-beans@users.noreply.github.com>
2024-03-31 17:43:29 +0300

    Update close_for_construction.py

--- close_for_construction.py
+++ close_for_construction.py
@@ -34,14 +34,14 @@ def save_earth():
     # outcome
     return A_BETTER_EARTH'''
 st.code(code)
-st.caption("**(The aboveis pseudo-code for decoration purposes. It's not supposed to make sense.)**")
+st.caption("**(The above is pseudo-code for decoration purposes. It's not supposed to make sense.)**")
 st.write(":green[**Agriculture**] **is a very dominant part of Ethiopia's economy, just like how it is with most other developing countries. We have recently started to use a more modern and technology-based approach when it comes to farming; and this project aims to play a part in that.**")
 st.write(":green[**Plantr**] **is a project that combines a chatGPT-like LLM (AI21), an image recognition API (Pl@ntNet) and geo-spatial technology to first recognize and identify plants from pictures and then give the user information about how the plant can be taken care of, how it could be used, etc. And then it gives analysis on how the addition of that species to a novel ecosystem could affect said ecosystem.**")
 
 st.divider()
 st.header("_Q: Why :green[plantr?]_")
 st.divider()
-st.write("**If you're talking about the name, I genuinely have no clue. It was a placeholder at first but then it just stuck, I guess. Like, c'mon, it's not _that_ bad.**")
+st.write("**If you're talking about the name, I genuinely have no clue. It was a placeholder at first but then it just stuck, I guess.**")
 st.write("**But the goal is to make software that can be used not only to protect and save ecosystems, but to :green[**enhance**] them, make them :green[**stronger**] and more :green[**efficient.**] It's based on the (almost-baseless, I admit) assumption that nature doesn't need to be constantly bombarded with fertilizers or 'care and protection' procedures (such as terracing or drone-watering) in order to yield food long-term.**")
 st.write(":green[**Ecosystems can fully sustain themselves**] **while also providing abundant food, but only if the organisms that consitute them** :green[**interact with each other in very specific ways.**]")
 st.write("**By analyzing climate, geo-location and plant growth requirement data and using it to :green[**simulate**] how selected food crops and plants would interact with each other and their environment, we can manage our farms more efficiently while making them low-maintainance, and :green[**eliminate the need for invasive human action.**] Thus making food :green[**healthier, cheaper**] and basically :green[**saving the Earth**] in the process.**")

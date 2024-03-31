c6d3d06da00861aec28e28677240bd8e8922b023
stevie <122219518+avocado-beans@users.noreply.github.com>
2024-03-31 17:11:02 +0300

    Update close_for_construction.py

--- close_for_construction.py
+++ close_for_construction.py
@@ -1,12 +1,10 @@
 import streamlit as st
 import streamlit_lottie
 
-st.header("PLANTR is temporarily down for maintainance!", divider='green')
-st.subheader("I'm modifying it until the day of the contest so it'll be down for a few days.")
-st.write("Thanks for visiting the site, though! Trust me, this project will not disappoint :)")
-st.write("I'm @tae2big on instagram if you want to contact me until then.")
-st.divider()
-st.image("images/farm.jpg")
+st.header(":green[PLANTR] is temporarily down for maintainance!", divider='green')
+st.subheader("**I'm modifying it until the day of the contest so it'll be down for a few days.**")
+st.write("**Thanks for visiting the site, though! Trust me, this project will not disappoint :)**")
+st.write("**I'm @tae2big on instagram if you want to contact me until then.**")
 st.divider()
 st.header("_Q: What is PLANTR?_")
 st.divider()

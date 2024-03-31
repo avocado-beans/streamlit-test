2bd076db3801228856ff4310b4b95ac513e0dd13
stevie <122219518+avocado-beans@users.noreply.github.com>
2024-03-31 16:47:03 +0300

    Update close_for_construction.py

--- close_for_construction.py
+++ close_for_construction.py
@@ -13,13 +13,14 @@ with right:
   st.header("Q: What is PLANTR?", divider='green')
   st.write("Agriculture is a very dominant part of Ethiopia's economy, just like how it is with most other developing countries. We have recently started to use a more modern and technology-based approach when it comes to farming; and this project aims to play a part in that.  \n  \nPlantr is a project that combines a chatGPT-like LLM (AI21), an image recognition API (Pl@ntNet) and geo-spatial technology to first recognize and identify plants from pictures and then give the user information about how the plant can be taken care of, how it could be used, etc. And then it gives analysis on how the addition of that species to a novel ecosystem could affect said ecosystem.")
 with left:
-  with st.echo():
-    st.lottie("https://lottie.host/6f984d1b-8857-4e15-b782-c5ea6db42352/pEyt6cbe53.json")
+  
+  st.lottie("https://lottie.host/6f984d1b-8857-4e15-b782-c5ea6db42352/pEyt6cbe53.json")
   
 st.divider()
 left, right = st.columns(2)
 with right:
-  st.lottie("https://lottie.host/43c9c9d4-94f6-4ce2-b652-9d3ccff533b0/gPLImv5Aoy.json")
+  with st.echo():
+    st.lottie("https://lottie.host/43c9c9d4-94f6-4ce2-b652-9d3ccff533b0/gPLImv5Aoy.json")
 with left:
   st.header("Q: Why?", divider='green')
   st.write("The goal is to make software that can be used not only to protect and save ecosystems, but to enhance them, make them stronger and more efficient. It's based on the (almost-baseless, I admit) assumption that nature doesn't need to be constantly bombarded with fertilizers or 'care and protection' procedures (such as terracing or drone-watering) in order to yield food long-term. ")

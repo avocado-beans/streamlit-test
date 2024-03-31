38821435099453a06e507c2c3da5c27537f6d5a7
stevie <122219518+avocado-beans@users.noreply.github.com>
2024-03-31 16:14:48 +0300

    Update close_for_construction.py

--- close_for_construction.py
+++ close_for_construction.py
@@ -5,7 +5,9 @@ st.write("Thanks for visiting the site, though! Trust me, this project will not 
 st.write("I'm @tae2big on instagram if you want to contact me until then.")
 st.subheader("Q: What is PLANTR?")
 st.write("Agriculture is a very dominant part of Ethiopia's economy, just like how it is with most other developing countries. We have recently started to use a more modern and technology-based approach when it comes to farming; and this project aims to play a part in that.  \n  \nPlantr is a project that combines a chatGPT-like LLM (AI21), an image recognition API (Pl@ntNet) and geo-spatial technology to first recognize and identify plants from pictures and then give the user information about how the plant can be taken care of, how it could be used, etc. And then it gives analysis on how the addition of that species to a novel ecosystem could affect said ecosystem.")
+st.write("The goal is to make software that can be used not only to protect and save ecosystems, but to enhance them, make them stronger and more efficient. It's based on the (almost-baseless, I admit) assumption that nature doesn't need to be constantly bombarded with fertilizers or "care and protection" procedures (such as terracing or drone-watering) in order to yield food long-term. Ecosystems can fully sustain themselves while also providing abundant food, but only if the organisms that consitute them interact with each other in very specific ways. By analyzing climate, geo-location and plant growth requirement data and using it to simulate how selected food crops and plants would interact with each other and their environment, we can manage our farms more efficiently while making them low-maintainance, and eliminate the need for invasive human action. Thus making food healthier, cheaper and basically saving the Earth in the process.")
+st.write("It's a long shot, but I think we can make it. Any support would help a lot.")
 st.divider()
 left, right = st.columns(2)
 with right:
-  st.caption("Love, Estifanos Tolemariam")
+  st.caption("(Platonic) Love, E. Tolemariam")

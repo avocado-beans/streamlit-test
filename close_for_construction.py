95352e03f915a547572cc84f30c4781ff516d1da
stevie <122219518+avocado-beans@users.noreply.github.com>
2024-03-31 17:48:30 +0300

    Update close_for_construction.py

--- close_for_construction.py
+++ close_for_construction.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import streamlit_lottie
 
+st.set_page_config(page_title="Closed For Construction | Plantr.", page_icon="🌿")
+
 st.title("Super sorry, dude! :green[Plantr] is temporarily down for :green[maintainance] and :green[development!]")
 st.lottie("https://lottie.host/84c0c93b-2d47-4441-8a72-3802742681df/2MjzLbcImk.json")
 st.divider()
